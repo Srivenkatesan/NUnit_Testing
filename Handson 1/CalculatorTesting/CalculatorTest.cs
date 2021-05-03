8f7cc048bc2dde1e66a603e109d174c276ff32b6
Srivenkatesan <82995705+Srivenkatesan@users.noreply.github.com>
2021-05-03 12:12:04 +0530

    Add files via upload

---
+++ Handson 1/CalculatorTesting/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CalculatorLib;
+
+namespace CalculatorTesting
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        [TestCase(10,20,30)]
+        public void CalculatorTestMethod(int numA,int numB,int expected)
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(numA, numB);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(0, 50, 0)]
+        public void CalculatorTestMethod2(int numA, int numB, int expected)
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(numA, numB);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
